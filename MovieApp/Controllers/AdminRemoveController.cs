97d8fa65698af9a88a4dd7a10da709001d56d60b
Rex Meyer <armeyer1998@gmail.com>
2021-12-04 09:27:02 -0500

    ignore files added

---
+++ MovieApp/Controllers/AdminRemoveController.cs
@@ -0,0 +1,31 @@
+﻿using MovieApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieApp.Controllers
+{
+    public class AdminRemoveController : ApiController
+    {
+
+
+
+        //public IHttpActionResult Delete (int id)
+        //{
+        //    var service = CreateAdminService();
+        //    if (!service.RemoveProfile(id))
+        //        return InternalServerError();
+        //    return Ok();
+        //}
+
+        //private AdminService CreateAdminService()
+        //{
+        //    var profileID = Guid.Parse(User.Identity.GetProfileId());
+        //    var adminService = new AdminService(profileID);
+        //    return adminService;
+        //}
+    }
+}
